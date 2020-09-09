1cc40406575da75e6895d9fba01d3775d6c95592
Adam Hoffman <68961086+adamhoffmang@users.noreply.github.com>
2020-09-09 16:18:51 -0700

    updates to GetHelpText text (#2)

--- src/bot/GetHelpTextUseCase.ts
+++ src/bot/GetHelpTextUseCase.ts
@@ -1,11 +1,16 @@
+import { LEADERBOARD, BUILD } from "./Commands";
+
 export class GetHelpUseCase {
   get(text: string): string {
     const botName = process.env.BOT_NAME;
     const command = text.split("help")[1];
 
-    if (command.match("leaderboard")) {
-      return `"@${botName} leaderboard" will output the current leaderboard`;
+    if (command.match(LEADERBOARD)) {
+      return `"@${botName} ${LEADERBOARD}" will output the current leaderboard`;
     }
-    return `"@${botName} help command" will output more info about that command.`;
+    if (command.match(BUILD)) {
+      return `"@${botName} ${BUILD}" will output the currently running KudoBot version number`;
+    }
+    return `Give Kudos by typing "@${botName} @Johnny Sample++ Great Job!" (where "Johnny Sample" is replaced by a mention of the person you would like to recognize)\n"@${botName} help command" will output more info about that command.\n Available commands: ${LEADERBOARD} and ${BUILD}`;
   }
 }
